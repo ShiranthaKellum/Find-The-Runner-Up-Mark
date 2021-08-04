dff1b1e0050c7b074a98fe4d7cbafff993cdbab8
ShiranthaKellum <73046831+ShiranthaKellum@users.noreply.github.com>
2021-08-04 11:07:05 +0530

    Update findTheRunnerUpMark.py

--- findTheRunnerUpMark.py
+++ findTheRunnerUpMark.py
@@ -1,5 +1,6 @@
 
 # This program finds the runne-up (the second large mark from a det of marks)
+# Explanation is in the readme file
 
 n = int (input ("Number of elements = "))
 
@@ -10,18 +11,11 @@ for i in range (n):                                         # get marks
     x = int (input ("Enter element : "))
     list.append (x)
 
-#for i in range (n):
-#   list.append (arr [i])
-
 list.sort ()                                                # sort the list
 
 for i in range (n-1):                                       
     if (list [n-1] == list [i]):
-        newlist.append (list [i])                           # insert same marks in to newList
-        #list.remove (list [i])
-
-#print (len (newlist))
-
+        newlist.append (list [i])                           # insert same marks in to newLis
 
 print (list [n-2-len(newlist)])                             # return the runner-up mark
 
